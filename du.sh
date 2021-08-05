721a876de52f0ca0b866ba1a21d7595493037683
root <root@siman.example.com>
2021-08-04 20:38:47 -0700

    test

---
+++ du.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dt=`date +%F`
+
+df -kh /> /tmp/du-$dt
+
+while read line
+do
+    fields=`echo $line | awk '{print NF}'`
+    case $fields in
+    1) echo -n "$dt $line ";;
+    5) echo -n "$dt "
+        echo $line | awk '{print $4}';;
+    6) echo -n "$dt "
+        echo $line | awk '{print $1,$3,$4,$5}';;
+    esac
+done < /tmp/du-$dt > /tmp/sto-$dt
